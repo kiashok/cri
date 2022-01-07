b2c6f65c0a847a84f4e5e9015be454afa27b495e
Daniel Canter <dcanter@microsoft.com>
2022-01-06 16:20:51 -0800

    Add DisableSameLayerUnpack option to cri config
    This change adds a new option named DisableSameLayerUnpack that stops
    Containerd from being able to unpack the same layer in parallel. This is
    most common if a pull of the same image is performed at the same time
    or different images that share layers being pulled at the same time.
    
    Signed-off-by: Daniel Canter <dcanter@microsoft.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -72,6 +72,13 @@ type ContainerdConfig struct {
 	// EnableLayerIntegrity is a boolean flag to specify whether to enable integrity
 	// protection of read-only container layers during image pull.
 	EnableLayerIntegrity bool `toml:"enable_layer_integrity" json:"enableLayerIntegrity"`
+
+	// DisableSameLayerUnpack changes the behavior when unpacking two or more of the
+	// same layers in parallel. If multiple snapshots are being made for the same layer,
+	// one will continue on to have a diff applied to it, and the other(s) will wait on the
+	// result of the first instead of all of them unpacking and the others simply getting garbage
+	// collected afterwards.
+	DisableSameLayerUnpack bool `toml:"disable_same_layer_unpack" json:"disableSameLayerUnpack"`
 }
 
 // CniConfig contains toml config related to cni
